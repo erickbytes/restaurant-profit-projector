037ac1c77fe76f1aa9e5017d725ddc7d0d23c244
erickbytes <erickrumbold@gmail.com>
2023-10-04 12:27:12 -0600

    added formula to show calulation method

--- breakeven.py
+++ breakeven.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Restaurant Breakeven Analysis")
-costs = st.number_input("Daily Restaurant Costs (Pesos)", key="costs", value=2700)
+st.title("Restaurant Profit Analysis")
+costs = st.number_input("Daily Restaurant Costs (Pesos)", key="costs", value=2700, step=50)
 corn_dogs_price = st.number_input("Corn Dogs Price (Pesos)", key="corn_dogs", value=35)
 onion_rings_price = st.number_input(
     "Onion Rings Price (Pesos)", key="onion_rings", value=35
@@ -13,10 +13,12 @@ profit = (
     (corn_dogs_sold * corn_dogs_price) + (onion_rings_sold * onion_rings_price) - costs
 )
 result = f"The restauraunt's projected daily profit is ${profit:,} pesos.".replace("$-","-$")
-st.write(result)
+st.subheader(result)
 data = {"profit": {"$ pesos": profit}}
 df = pd.DataFrame.from_dict(data)
 if "-" in str(profit):
     st.bar_chart(df, color="#a30311")
 else:
     st.bar_chart(df, color="#0c9618")
+st.subheader("How profit is calculated:")
+st.text("profit = (corn dogs sold * corn dogs price) + (onion rings sold * onion rings price) -  costs")
