014227497d3f4e586ddcba87e7cbc2587c8b4db5
erickbytes <erickrumbold@gmail.com>
2023-10-04 12:03:43 -0600

    added conditional colors for negative or positive profit

--- breakeven.py
+++ breakeven.py
@@ -2,18 +2,21 @@ import pandas as pd
 import streamlit as st
 
 st.title("Restaurant Breakeven Analysis")
-costs = st.number_input("Daily Restaurant Cost (Pesos)", key="costs", value=2700)
+costs = st.number_input("Daily Restaurant Costs (Pesos)", key="costs", value=2700)
 corn_dogs_price = st.number_input("Corn Dogs Price (Pesos)", key="corn_dogs", value=35)
 onion_rings_price = st.number_input(
     "Onion Rings Price (Pesos)", key="onion_rings", value=35
 )
-corn_dogs = st.slider("Corn Dogs Orders Sold", value=30)
-onion_rings = st.slider("Onion Rings Orders Sold", value=20)
-st.write(f"Assumed Total Daily Restaurant Cost (Pesos): {costs}")
+corn_dogs_sold = st.slider("Corn Dogs Orders Sold", value=30)
+onion_rings_sold = st.slider("Onion Rings Orders Sold", value=20)
 profit = (
-    (corn_dogs * corn_dogs_price) + (onion_rings * onion_rings_price) - costs
+    (corn_dogs_sold * corn_dogs_price) + (onion_rings_sold * onion_rings_price) - costs
 )
-st.write(f"The restauraunt's projected daily profit is ${profit:,} pesos.")
+result = f"The restauraunt's projected daily profit is ${profit:,} pesos.".replace("$-","-$")
+st.write(result)
 data = {"profit": {"$ pesos": profit}}
 df = pd.DataFrame.from_dict(data)
-st.bar_chart(df, color="#0c9618")
+if "-" in str(profit):
+    st.bar_chart(df, color="#a30311")
+else:
+    st.bar_chart(df, color="#0c9618")
