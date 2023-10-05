30aa08461645a528ba79361c1896a9cc9254859f
erickbytes <erickrumbold@gmail.com>
2023-10-04 18:49:13 -0600

    added a 3rd column and multiple corn dog options

--- breakeven.py
+++ breakeven.py
@@ -3,20 +3,37 @@ import streamlit as st
 
 # https://docs.streamlit.io/library/api-reference
 st.title("Restaurant Profit Analysis")
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
-    costs = st.number_input("Daily Restaurant Costs (Pesos)", key="costs", value=2700, step=50)
-    corn_dogs_price = st.number_input("Corn Dogs Price (Pesos)", key="corn_dogs", value=35)
+    costs = st.number_input(
+        "Daily Restaurant Costs (Pesos)", key="costs", value=2700, step=50
+    )
+    four_corn_dogs_price = st.number_input(
+        "Corn Dogs Price x 4 (Pesos)", key="four_corn_dogs", value=35
+    )
+    six_corn_dogs_price = st.number_input(
+        "Corn Dogs Price x 6 (Pesos)", key="six_corn_dogs", value=50
+    )
+    come_todo_corn_dogs_price = st.number_input(
+        "Corn Dogs Price x Come Todo (Pesos)", key="todo_corn_dogs", value=80
+    )
     onion_rings_price = st.number_input(
         "Onion Rings Price (Pesos)", key="onion_rings", value=35
     )
-    corn_dogs_sold = st.slider("Corn Dogs Orders Sold", value=30)
+with col2:
+    four_corn_dogs_sold = st.slider("Corn Dogs x 4 Orders Sold", value=20)
+    six_corn_dogs_sold = st.slider("Corn Dogs x 6 Orders Sold", value=15)
+    come_todo_corn_dogs_sold = st.slider("Corn Dogs x Come Todo Orders Sold", value=5)
     onion_rings_sold = st.slider("Onion Rings Orders Sold", value=20)
     profit = (
-        (corn_dogs_sold * corn_dogs_price) + (onion_rings_sold * onion_rings_price) - costs
+        (four_corn_dogs_sold * four_corn_dogs_price)
+        + (six_corn_dogs_sold * six_corn_dogs_price)
+        + (come_todo_corn_dogs_sold * four_corn_dogs_price)
+        + (onion_rings_sold * onion_rings_price)
+        - costs
     )
-    result = f"${profit:,}".replace("$-","-$")
-with col2:
+    result = f"${profit:,}".replace("$-", "-$")
+with col3:
     st.metric("pesos profit", result)
     data = {"profit": {"$ pesos": profit}}
     df = pd.DataFrame.from_dict(data)
@@ -25,4 +42,6 @@ with col2:
     else:
         st.bar_chart(df, color="#0c9618")
 st.subheader("How profit is calculated:")
-st.text("profit = (corn dogs sold x corn dogs price) + (onion rings sold x onion rings price) -  costs")
+st.text(
+    "profit = (corn dogs sold x corn dogs price) + (onion rings sold x onion rings price) -  costs"
+)
